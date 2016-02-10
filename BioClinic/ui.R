952592f84a91e41d9c0734f01d0970ec5a1daf5c
Alba Sanchis-Juan <alba.sanchis@uv.es>
2016-02-10 20:42:12 +0100

    Update ui.R

--- BioClinic/ui.R
+++ BioClinic/ui.R
@@ -10,8 +10,9 @@ shinyUI(fluidPage(
     
     sidebarPanel(
       conditionalPanel(
-        'input.dataset == "index"',
-          h3("General info")
+        'input.dataset == "index"'
+        , h3("General info")
+        , hr()
         , fileInput('analysis_file', 'Choose file to upload',
                     accept = c(
                       'text/csv',
@@ -21,23 +22,23 @@ shinyUI(fluidPage(
                       '.csv',
                       '.tsv'
                     ))
-        , hr()
       ),
       conditionalPanel(
-        'input.dataset == "boxplot"',
-        h3("Boxplot Analysis"),
-        # , shinyFilesButton("analysis_file", "Load File", "Please select a file", FALSE)
-        
-        h4("Select your variable to plot")
+        'input.dataset == "boxplot"'
+        , h3("Boxplot Analysis")
+        , hr()
+        , h4("Select your variable to plot")
         , uiOutput("variables_list")
         , uiOutput("interval_age")
         , uiOutput("interval_imc")
-        , radioButtons("sex", 'Sex', choices = c("All", "Male", "Female"), selected = c("All"))
-        , helpText("Underweight:  <18.5, Normal Range: 18.5-24.99, Moderately obsese: >25, Severely Obese: >30")
+        , uiOutput("sex_option")
+        , uiOutput("helpmessage")
       ),
       conditionalPanel(
-        'input.dataset == "scatterplot"',
-          h3("Select your variables to compare in the scatterplot")
+        'input.dataset == "scatterplot"'
+        , h3("Scatterplot Analysis")
+        , hr()
+        , h4("Select your variables to compare in the scatterplot")
         , uiOutput("x_axis")
         , uiOutput("y_axis")
       )
@@ -45,7 +46,7 @@ shinyUI(fluidPage(
     mainPanel(
       tabsetPanel(
         id = 'dataset',
-        tabPanel('index', textOutput("Blah")),
+        tabPanel('index', textOutput("index_message")),
         tabPanel('boxplot', plotOutput("boxplot")),
         tabPanel('scatterplot', plotOutput("scatterplot"))
       )
