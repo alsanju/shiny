374aa4c217ccc8fc6704b7d9ae1175254ccf6b56
Alba Sanchis-Juan <alba.sanchis@uv.es>
2016-02-10 20:07:12 +0100

    Update ui.R

--- AniPlots/ui.R
+++ AniPlots/ui.R
@@ -9,27 +9,46 @@ shinyUI(fluidPage(
   sidebarLayout(
     
     sidebarPanel(
-      h3("Analysis")
-      # , shinyFilesButton("analysis_file", "Load File", "Please select a file", FALSE)
-      , fileInput('analysis_file', 'Choose file to upload',
-                  accept = c(
-                    'text/csv',
-                    'text/comma-separated-values',
-                    'text/tab-separated-values',
-                    'text/plain',
-                    '.csv',
-                    '.tsv'
-                  ))
-      , hr()
-      , h4("Select your variable to plot")
-      , uiOutput("variables_list")
-      , uiOutput("interval_age")
-      , uiOutput("interval_imc")
-      , helpText("Underweight:  <18.5, Normal Range: 18.5-24.99, Moderately obsese: >25, Severely Obese: >30")
-      
+      conditionalPanel(
+        'input.dataset == "index"',
+          h3("General info")
+        , fileInput('analysis_file', 'Choose file to upload',
+                    accept = c(
+                      'text/csv',
+                      'text/comma-separated-values',
+                      'text/tab-separated-values',
+                      'text/plain',
+                      '.csv',
+                      '.tsv'
+                    ))
+        , hr()
+      ),
+      conditionalPanel(
+        'input.dataset == "boxplot"',
+        h3("Boxplot Analysis"),
+        # , shinyFilesButton("analysis_file", "Load File", "Please select a file", FALSE)
+        
+        h4("Select your variable to plot")
+        , uiOutput("variables_list")
+        , uiOutput("interval_age")
+        , uiOutput("interval_imc")
+        , radioButtons("sex", 'Sex', choices = c("All", "Male", "Female"), selected = c("All"))
+        , helpText("Underweight:  <18.5, Normal Range: 18.5-24.99, Moderately obsese: >25, Severely Obese: >30")
+      ),
+      conditionalPanel(
+        'input.dataset == "scatterplot"',
+          h3("Select your variables to compare in the scatterplot")
+        , uiOutput("x_axis")
+        , uiOutput("y_axis")
+      )
     ),
     mainPanel(
-      plotOutput("boxplot")
+      tabsetPanel(
+        id = 'dataset',
+        tabPanel('index', textOutput("Blah")),
+        tabPanel('boxplot', plotOutput("boxplot")),
+        tabPanel('scatterplot', plotOutput("scatterplot"))
+      )
     )
   )
 ))
